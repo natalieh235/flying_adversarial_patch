e628007cf2a0ae5d86f969c9b474e0c14cf0fc04
Pia Hanfeld <pia.hanfeld@gmail.com>
2022-11-14 10:47:51 +0100

    added fix to patch class and test on targeted attack

--- adversarial_frontnet/attacks.py
+++ adversarial_frontnet/attacks.py
@@ -10,25 +10,18 @@ from patch_placement import place_patch
 class Patch(torch.nn.Module):
     def __init__(self, patch_size=[1, 50, 50]):
         super(Patch, self).__init__()
-        patch = (torch.rand(1, *patch_size) * 255.).requires_grad_()
-        self.patch = torch.nn.Parameter(patch)
+        self.patch = (torch.rand(1, *patch_size) * 255.)#.requires_grad_()
+        #self.patch = torch.nn.Parameter(patch)
 
-        # init random distribution samplers for initial random pose of the patch
-        # patch is placed using an angle, a scale factor and a translation vector
-        angle_sampler = torch.distributions.uniform.Uniform(np.radians(-45), np.radians(45))
-        scale_sampler = torch.distributions.uniform.Uniform(0.01, 0.5)
-        translation_sampler = torch.distributions.uniform.Uniform(0.0, 1.0)
+        transformation_min = torch.tensor([[[*torch.rand(3,)],[*torch.rand(3,)] , [0, 0, 1]]])
+        transformation_max = torch.tensor([[[*torch.rand(3,)],[*torch.rand(3,)] , [0, 0, 1]]])
 
-        # generate two lists of patch placement parameters
-        patch_parameters_min = torch.tensor([angle_sampler.sample(), scale_sampler.sample(), *translation_sampler.sample((2,))]).requires_grad_(True)
-        patch_parameters_max = torch.tensor([angle_sampler.sample(), scale_sampler.sample(), *translation_sampler.sample((2,))]).requires_grad_(True)
-
-        self.patch_parameters_min = torch.nn.Parameter(patch_parameters_min)
-        self.patch_parameters_max = torch.nn.Parameter(patch_parameters_max)
+        self.transformation_min = torch.nn.Parameter(transformation_min)
+        #self.transformation_max = torch.nn.Parameter(transformation_max)
 
     def place(self, image):
-        image_min = place_patch(image, self.patch, *self.patch_parameters_min)
-        image_max = place_patch(image, self.patch, *self.patch_parameters_max)
+        image_min = place_patch(image, self.patch, self.transformation_min)
+        image_max = place_patch(image, self.patch, self.transformation_max)
         return torch.stack([image_min, image_max])
 
     def batch_place(self, batch):
@@ -46,8 +39,6 @@ class TargetedAttack():
 
         # define the maximum and minimum targets to optimize towards
         self.target = torch.tensor(target)
-        # self.target_min = self.pose * ~target
-        # self.target_max = self.pose * ~target + (3*target.int())   # TODO: check if this makes sense for y and z
 
         # init random patch
         self.x_patch = Patch()
@@ -64,7 +55,7 @@ class TargetedAttack():
                     input_images_min, input_images_max = self.x_patch.batch_place(image_batch)
 
                     targets_min = pose_batch * ~self.target
-                    targets_max = pose_batch * self.target + (3*self.target.int())
+                    targets_max = pose_batch * self.target + (4*self.target.int())
 
                     # get prediction of current pose from NN
                     pred_attack_min = torch.stack(self.model(input_images_min))
@@ -109,6 +100,46 @@ class TargetedAttack():
 
         return self.x_patch
 
+def targeted_attack(image, target, model, path="eval/targeted/"):
+    # initialize optimizer
+    x_patch = Patch()
+    opt = torch.optim.Adam(x_patch.parameters(), lr=1e-4)
+    prediction = torch.concat(model(image)).squeeze(1)
+    
+    new_image = place_patch(image, x_patch.patch, x_patch.transformation_min)
+    pred_attack = torch.concat(model(new_image)).squeeze(1)
+    loss = torch.dist(prediction, pred_attack, p=2)
+
+    losses = []
+    losses.append(loss.detach().numpy())
+
+    i = 0.
+    try:
+        while loss > 0.01:
+            i += 1
+            prediction = torch.concat(model(new_image)).squeeze(1)
+            loss = torch.dist(prediction, target, p=2)
+            losses.append(loss.detach().numpy())
+            opt.zero_grad()
+            loss.backward()
+            opt.step()
+
+            x_patch.patch.data.clamp_(0., 255.)
+            new_image = place_patch(image, x_patch.patch, x_patch.transformation_min)
+            if i % 100 == 0:
+                print("step %d, loss %.6f" % (i, loss))
+                print("transformation matrix: ", x_patch.transformation_min.detach().numpy())
+    except KeyboardInterrupt:
+        print("Aborting optimization...")    
+
+    print("Bing!")
+    print("Last loss: ", loss.detach().numpy())
+    print("Last prediciton: ", pred_attack)
+
+    np.save(path+'losses_test', losses)
+
+    return x_patch.patch, x_patch.transformation_min
+
 
 def untargeted_attack(image, patch, model, transformation_matrix, path='eval/untargeted/'): # angle, scale, tx, ty,
     # initialize optimizer
@@ -152,47 +183,6 @@ def untargeted_attack(image, patch, model, transformation_matrix, path='eval/unt
     return patch, transformation_matrix #[angle, scale, tx, ty]
 
 if __name__=="__main__":
-    # import matplotlib.pyplot as plt
-    # import os
-
-    # from util import load_dataset, load_model
-    # model_path = '../pulp-frontnet/PyTorch/Models/Frontnet160x32.pt'
-    # model_config = '160x32'
-    # dataset_path = '../pulp-frontnet/PyTorch/Data/160x96OthersTrainsetAug.pickle'
-
-    # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-    # model = load_model(path=model_path, device=device, config=model_config)
-    # model.eval()
-    # dataset = load_dataset(path=dataset_path, batch_size=32, shuffle=True, drop_last=True, num_workers=0)
-
-    # path = 'eval/targeted_2_sides/'
-    # os.makedirs(path, exist_ok = True)
-
-    # image, pose = dataset.dataset.__getitem__(0)
-    # image = image.unsqueeze(0)
-    # attack = TargetedAttack(model, dataset, path=path)
-
-    # np.save(path+'ori_patch', attack.x_patch.patch.detach().numpy())
-
-    # print("Original pose: ", pose, pose.shape)
-    # prediction = torch.concat(model(image)).squeeze(1)
-    # print("Predicted pose: ", prediction)
-    # print("L2 dist original-predicted: ", torch.dist(pose, prediction, p=2))
-
-    # optimized_x_patch = attack.optimize()
-    
-    # np.save(path+"opti_patch", optimized_x_patch.patch.detach().numpy())
-    # new_image_right = place_patch(image, optimized_x_patch.patch, *optimized_x_patch.patch_parameters_min)
-
-    # plt.imshow(new_image_right[0][0].detach().numpy(), cmap='gray')
-    # plt.show()
-
-    # new_image_left = place_patch(image, optimized_x_patch.patch, *optimized_x_patch.patch_parameters_max)
-
-    # plt.imshow(new_image_left[0][0].detach().numpy(), cmap='gray')
-    # plt.show()
-
     import matplotlib.pyplot as plt
     import os
 
@@ -207,53 +197,33 @@ if __name__=="__main__":
     model.eval()
     dataset = load_dataset(path=dataset_path, batch_size=32, shuffle=True, drop_last=True, num_workers=0)
 
-    path = 'eval/untargeted/'
+    path = 'eval/targeted_test/'
     os.makedirs(path, exist_ok = True)
 
-    # calculate random translation, rotation (in radians), and scale factor
-    # tx = torch.randint(high=2, size=(1,)).float().requires_grad_()    # TODO: fix translation such that patch is always visible
-    # ty = torch.randint(high=2, size=(1,)).float().requires_grad_()
-    # rotation = torch.distributions.uniform.Uniform(np.radians(-45), np.radians(45)).sample().requires_grad_()  # PyTorch doesn't offer a radians function yet
-    # scale = torch.distributions.uniform.Uniform(0.01, 0.7).sample().requires_grad_()
-
-    # tx = torch.tensor([0.2]).float().requires_grad_()
-    # ty = torch.tensor([0.1]).float().requires_grad_()
-    # rotation = torch.tensor(np.radians(35.76629)).requires_grad_()
-    # scale = torch.tensor(0.3).requires_grad_()
-
-    # print("Rotation: ", np.degrees(rotation.detach().numpy()))
-    # print("Scale: ", scale.detach().numpy())
-    # print("Tx: ", tx.detach().numpy(), "Ty: ", ty.detach().numpy())
-
     image, pose = dataset.dataset.__getitem__(0)
-    patch = (torch.rand(1, 1, 50, 50) * 255.).requires_grad_()
-    patch_copy = patch.detach().clone()
-    np.save(path+'ori_patch', patch_copy.numpy())
-
     image = image.unsqueeze(0)
+    
+    #attack = TargetedAttack(model, dataset, path=path)
+    
+
+    #np.save(path+'ori_patch', attack.x_patch.patch.detach().numpy())
 
     print("Original pose: ", pose, pose.shape)
     prediction = torch.concat(model(image)).squeeze(1)
     print("Predicted pose: ", prediction)
     print("L2 dist original-predicted: ", torch.dist(pose, prediction, p=2))
+
+    optimized_x_patch, optimized_transformation = targeted_attack(image, torch.tensor([4., *prediction[1:]]), model, path)
+    #optimized_x_patch = attack.optimize()
     
-    # place the patch
-    transformation_matrix = torch.tensor([[[*torch.rand(3,)],[*torch.rand(3,)] , [0, 0, 1]]]).requires_grad_(True)
-    new_image = place_patch(image, patch, transformation_matrix)
-    plt.imshow(new_image[0][0].detach().numpy(), cmap='gray')
+    #np.save(path+"opti_patch", optimized_x_patch.patch.detach().numpy())
+    #new_image_right = place_patch(image, optimized_x_patch.patch, *optimized_x_patch.patch_parameters_min)
+    new_image_right = place_patch(image, optimized_x_patch, optimized_transformation)
+
+    plt.imshow(new_image_right[0][0].detach().numpy(), cmap='gray')
     plt.show()
 
-    pred_attack = torch.concat(model(new_image)).squeeze(1)
-    print("Predicted pose after attack: ", pred_attack)
+    # new_image_left = place_patch(image, optimized_x_patch.patch, *optimized_x_patch.patch_parameters_max)
 
-    print("L2 dist predicted-attack: ", torch.dist(prediction, pred_attack, p=2))
-
-    # target = torch.tensor([0.0, -0.7274,  0.3108, -0.1638])
-    # print("Target: ", target, target.shape )
-
-    optimized_patch, optimized_transformation_matrix = untargeted_attack(image, patch, model, transformation_matrix)
-    np.save(path+"opti_patch", optimized_patch.detach().numpy())
-    new_image = place_patch(image, patch, optimized_transformation_matrix)
-
-    plt.imshow(new_image[0][0].detach().numpy(), cmap='gray')
-    plt.show()
+    # plt.imshow(new_image_left[0][0].detach().numpy(), cmap='gray')
+    # plt.show()
