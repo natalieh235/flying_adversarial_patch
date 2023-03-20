32cfcbea53307842cff757a95899583e6e53221f
Wolfgang Hoenig <hoenig@tu-berlin.de>
2023-03-20 08:11:58 +0100

    exp1 improvements

--- adversarial_frontnet/exp1.py
+++ adversarial_frontnet/exp1.py
@@ -8,6 +8,7 @@ import subprocess
 import signal
 import os
 import numpy as np
+from collections import defaultdict
 
 def run_attack(settings):
     with tempfile.TemporaryDirectory() as tmpdirname:
@@ -25,6 +26,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--file', default='exp1.yaml')
     parser.add_argument('--norun', action='store_true')
+    parser.add_argument('-j', default=4)
     args = parser.parse_args()
 
     signal.signal(signal.SIGTERM, on_sigterm)
@@ -39,20 +41,26 @@ def main():
         for mode in ['fixed', 'split', 'joint', 'hybrid']:
             s = copy.copy(base_settings)
             s['mode'] = mode
-            s['path'] = "eval/exp1/" + mode + str(i)
+            s['path'] = "eval/exp1a/" + mode + str(i)
             all_settings.append(s)
 
     if not args.norun:
         # start 4 worker processes
-        with Pool(processes=4) as pool:
+        with Pool(processes=args.j) as pool:
             for i in pool.imap_unordered(run_attack, all_settings):
                 pass
 
     # output statistics
+    result = defaultdict(list)
     for settings in all_settings:
         p = Path(settings['path'])
         test_losses = np.load(p / 'losses_test.npy')
-        print(settings['mode'], test_losses[-1])
+        # print(settings['mode'], test_losses[-1])
+        result[settings['mode']].append(test_losses[-1])
+
+    for k, v in result.items():
+        all = np.stack(v)
+        print(k, "mean", np.mean(all), "std", np.std(all))
 
 
 if __name__ == '__main__':
