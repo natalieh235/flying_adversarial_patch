f37998bd0bf2f9645212684109fdfe52ca78b659
Pia Hanfeld <pia.hanfeld@gmail.com>
2023-06-22 19:00:24 +0200

    simplified policy working for changes in x and y direction

--- hardware/frontnet_ros/frontnet_ros/attacker_policy.py
+++ hardware/frontnet_ros/frontnet_ros/attacker_policy.py
@@ -2,21 +2,23 @@ import numpy as np
 import rowan
 
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
 
 # black_p = np.zeros((1, 96, 160))
 # white_p = np.ones((1, 96, 160))
 # patches = np.stack([black_p, white_p])
 
+# def gen_circle(r, n):
+
+
 
 # all target poses 
-target_positions = np.array([[1, 1, 0], [1, -1, 0]])
+target_positions = np.array([[1, 0, 0], [2, 0, 0]]) # [1, 1, 0], [1, -1, 0], 
 num_targets = len(target_positions)
 
 # example opimized position norm
-positions_p0 = np.array([[0.5, -0.6, 0.4], [0.3, -0.2, -1.0]])
-positions_p1 = np.array([[0.5, -0.8, 0.3], [0.5, 0.6, 0.4]])
-positions = np.stack([positions_p0, positions_p1])
+# positions_p0 = np.array([[0.5, -0.6, 0.4], [0.3, -0.2, -1.0]])
+# positions_p1 = np.array([[0.5, -0.8, 0.3], [0.5, 0.6, 0.4]])
+# positions = np.stack([positions_p0, positions_p1])
 
 # current victim pose
 victim_pose = np.array([[-1., 0., 0., 0.],        # initial pose in world frame 
@@ -25,8 +27,9 @@ victim_pose = np.array([[-1., 0., 0., 0.],        # initial pose in world frame
 
 
 # desired next victim pose in world frame
-desired_pose = np.array([[-1., 0., 0., 0.],         # desired pose in world frame 
-                            [0., -1., 0., 1.],         # y = 1
+# desired_trajectory = 
+desired_pose = np.array([[-1., 0., 0., 2.],         # desired pose in world frame 
+                            [0., -1., 0., 0.],         # y = 1
                             [0., 0., 1., 1.]])         # no rotation, z = 1
 
 # assignment
@@ -57,22 +60,35 @@ for i in range(10):
     # print(possible_new_positions)
 
     for target_idx in range(num_targets):
-        # print("target: ", target_positions[target_idx])
-        # print("victim_pos: ", victim_pos)
-        new_pos = rowan.rotate(victim_quat, target_positions[target_idx])
-        # print("new_pos:", new_pos)
-        heading_vec = [1*np.cos(desired_yaw), 1*np.sin(desired_yaw), 0.]
-        # print("heading_vec:", heading_vec)
+        # since we're keeping the yaw angle of the victim fixed, 
+        # we can simply compute the max. achievable new position in world coordinates:
+        max_new_pos = victim_pos + target_positions[target_idx]
+        max_new_pos[0] -= 1. # safety radius
 
-        new_pos_world = new_pos + victim_pos + heading_vec
-        # print("new_pos in world: ", new_pos_world)
         new_pose_world = np.zeros((3, 4))
         new_pose_world[:3, :3] = rowan.to_matrix(victim_quat)
-        new_pose_world[..., 3][:3] = new_pos_world 
-        # print("new pose in world: ", new_pose_world)
+        new_pose_world[..., 3][:3] = max_new_pos
+
         possible_new_positions[target_idx, :, :] = new_pose_world
 
-    # print(possible_new_positions)
+
+    # maybe not even needed
+    #     print("target: ", target_positions[target_idx])
+    #     print("victim_pos: ", victim_pos)
+    #     new_pos = rowan.rotate(victim_quat, target_positions[target_idx])
+    #     print("new_pos:", new_pos)
+    #     heading_vec = [1*np.cos(desired_yaw), 1*np.sin(desired_yaw), 0.]
+    #     print("heading_vec:", heading_vec)
+
+    #     new_pos_world = new_pos + victim_pos + heading_vec
+    #     print("new_pos in world: ", new_pos_world)
+    #     new_pose_world = np.zeros((3, 4))
+    #     new_pose_world[:3, :3] = rowan.to_matrix(victim_quat)
+    #     new_pose_world[..., 3][:3] = new_pos_world 
+    #     # print("new pose in world: ", new_pose_world)
+    #     possible_new_positions[target_idx, :, :] = new_pose_world
+
+    # # print(possible_new_positions)
 
     distances = [np.linalg.norm(possible_new_positions[target_idx]-desired_pose, ord=2) for target_idx in range(num_targets)]
     print("l2 dist to desired: ", distances)
@@ -86,7 +102,7 @@ for i in range(10):
     # print(f"Presenting patch {m} at position {positions[m, k]} (in image frame)")
 
     position_change = possible_new_positions[k] - victim_pose
-    victim_pose[:2, 3] += 0.3*(position_change[:2, 3])
+    victim_pose[:2, 3] += (0.3*(position_change[:2, 3]) + np.random.uniform(-0.2, 0.2, (2,))) # add a bit of uncertainty
     print("updated victim pose: ", victim_pose)
     dist_after_update = np.linalg.norm((victim_pose-desired_pose), ord=2)
     print("error to desired: ", dist_after_update)
@@ -105,25 +121,11 @@ all_desired_positions = np.array(all_desired_positions)
 
 
 fig, ax = plt.subplots()
+ax.set_xlim(-2, 2)
+ax.set_ylim(-2., 2)
 ax.plot(all_victim_positions.T[1], all_victim_positions.T[0], marker=(3, 0, desired_yaw), markersize=20, linestyle='None')
 for i, (a,b) in enumerate(zip(all_victim_positions.T[1], all_victim_positions.T[0])): 
     plt.text(a, b-0.007, str(i))
 
 ax.plot(all_desired_positions.T[1], all_desired_positions.T[0], marker=(3, 0, desired_yaw), markersize=20, linestyle='None')
 plt.show()
-
-# ax.set_xlim(-5., 0.2)
-# ax.set_ylim(-0.2, 0.2)
-
-# current_plot = ax.scatter([], [], c='red', label='Current Position')
-# desired_plot = ax.scatter([], [], c='blue', label='Desired Position')
-
-# def update(frame):
-#     current_plot.set_offsets([all_victim_positions[frame]])
-#     desired_plot.set_offsets([all_desired_positions[frame]])
-
-# animation = FuncAnimation(fig, update, frames=len(all_victim_positions), interval=200)
-
-# ax.legend()
-
-# plt.show()
