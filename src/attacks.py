ee9c4d02217cabd5d986bf8286a6ae90e6619b41
Natalie Huang <natalie.huang99@gmail.com>
2024-07-17 16:06:47 +0200

    camera testing

--- src/attacks.py
+++ src/attacks.py
@@ -152,9 +152,14 @@ def targeted_attack_joint(dataset, patch, model, positions, assignment, targets,
                         mod_img.clamp_(0., 255.)
 
                         # predict x, y, z, yaw
-                        # x, y, z, phi = model(mod_img)
-                        results = model(mod_img)
-                        results = results.pandas().xyxy[0].to_dict(orient="records")
+                        x, y, z, phi = model(mod_img)
+                        print(x, y, z)
+                        print(type(x), type(y), type(z))
+                        # results = model(mod_img).xyxy[0]
+                        # results = results.pandas().xyxy[0].to_dict(orient="records")
+                        # for data in results:
+                        #     if data[5] == 0: # this is a person
+                        #          xmin, ymin, xmax, ymax = data[:4]
 
                         # target_losses.append(torch.mean(all_l2))
                         # prepare shapes for MSE loss
@@ -512,21 +517,21 @@ if __name__=="__main__":
 
     # model = load_model(path=model_path, device=device, config=model_config)
     # model.eval()
-    # print("Loading quantized network? ", quantized)
-    # if not quantized:
-    #     # load full-precision network
-    #     from util import load_model
-    #     model_path = 'pulp-frontnet/PyTorch/Models/Frontnet160x32.pt'
-    #     model_config = '160x32'
-    #     model = load_model(path=model_path, device=device, config=model_config)
-    # else:
-    #     # load quantized network
-    #     from util import load_quantized
-    #     model_path = 'misc/Frontnet.onnx'
-    #     model = load_quantized(path=model_path, device=device)
+    print("Loading quantized network? ", quantized)
+    if not quantized:
+        # load full-precision network
+        from util import load_model
+        model_path = 'pulp-frontnet/PyTorch/Models/Frontnet160x32.pt'
+        model_config = '160x32'
+        model = load_model(path=model_path, device=device, config=model_config)
+    else:
+        # load quantized network
+        from util import load_quantized
+        model_path = 'misc/Frontnet.onnx'
+        model = load_quantized(path=model_path, device=device)
     
     # model.eval()
-    model = torch.hub.load("ultralytics/yolov5", "yolov5s")  # Can be 'yolov5n' - 'yolov5x6', or 'custom'
+    # model = torch.hub.load("ultralytics/yolov5", "yolov5s")  # Can be 'yolov5n' - 'yolov5x6', or 'custom'
 
     train_set = load_dataset(path=dataset_path, batch_size=batch_size, shuffle=True, drop_last=False, num_workers=0)
     # train_set.dataset.data.to(device)   # TODO: __getitem__ and next(iter(.)) are still yielding data on cpu!
--- src/camera.py
+++ src/camera.py
@@ -9,6 +9,7 @@ import rowan
 import torch
 
 import cv2
+import csv
 
 from util import opencv2quat, load_dataset
 
@@ -27,15 +28,29 @@ class Camera:
         
         rvec = np.array(camera_config['rvec'])
         tvec = camera_config['tvec']
-
+        self.make_extrinsic(rvec, tvec)
+    
+    def make_extrinsic(self, rvec, tvec):
         self.camera_extrinsic = np.zeros((4,4))
         self.camera_extrinsic[:3, :3] = rowan.to_matrix(opencv2quat(rvec))
         self.camera_extrinsic[:3, 3] = tvec
         self.camera_extrinsic[-1, -1] = 1.
-
-        print('extrinsic', np.shape(self.camera_extrinsic))
-        print('intrinsic', np.shape(self.camera_intrinsic))
     
+    def update_with_points(self, objs, imgs, img_size):
+        print('updating matrix')
+        obj_pts = np.array(objs, dtype=np.float32)[np.newaxis]
+        img_pts = np.array(imgs, dtype=np.float32)[np.newaxis]
+        cam_matrix = np.zeros((3, 3))
+        ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(obj_pts, img_pts, img_size, cameraMatrix=self.camera_intrinsic, distCoeffs=self.distortion_coeffs, flags=cv2.CALIB_USE_INTRINSIC_GUESS)
+        # ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(obj_pts, img_pts, img_size, cam_matrix, None, flags=cv2.CALIB_USE_INTRINSIC_GUESS)
+        # print(mtx)
+        print('new matrix', mtx)
+        print('rvecs', rvecs)
+        print('tvecs', tvecs)
+        self.camera_intrinsic = mtx
+        self.distortion_coeffs = dist
+        self.make_extrinsic(rvecs[0].squeeze(), tvecs[0].squeeze())
+
     # compute relative position of center of patch in camera frame
     def xyz_from_bb(self, bb):
         # bb - xmin,ymin,xmax,ymax
@@ -63,11 +78,10 @@ class Camera:
         ac = (a1+a2)/2
         # get the position
         xyz = distance*ac/np.linalg.norm(ac)
-        return xyz
+        new_xyz = (np.linalg.inv(self.camera_extrinsic) @ [*xyz, 1])[:3]
+        return new_xyz
 
     def point_from_xyz(self, coords):
-        # print('coords shape', np.shape(coords), np.shape(np.array([1])))
-        # print(np.shape(self.camera_extrinsic))
         camera_frame = self.camera_extrinsic @ coords
         image_frame = self.camera_intrinsic @ camera_frame[:3]
         u, v, w = image_frame
@@ -75,48 +89,81 @@ class Camera:
         img_y = int(np.round(v/w, decimals=0))
         return (img_x, img_y)
 
+def gen_camera_matrix(cam):
+    data = csv.DictReader(open('src/camera_calibration/ground_truth_pose.csv', mode='r'))
+    obj_pts = []
+    img_pts = []
+    img_size = (96, 160)
+    for d in data:
+        obj_pts.append([(d['x']), (d['y']), (d['z'])])
+        img_pts.append([d['img_x'], d['img_y']])
+
+    cam.update_with_points(obj_pts, img_pts, img_size)
 
 def test_camera():
     dataset_path = 'pulp-frontnet/PyTorch/Data/160x96StrangersTestset.pickle'
-    cam_config = 'misc/camera_calibration/calibration.yaml'
     model = torch.hub.load("ultralytics/yolov5", "yolov5s")  # Can be 'yolov5n' - 'yolov5x6', or 'custom'
+    cam_config = 'misc/camera_calibration/calibration.yaml'
     cam = Camera(cam_config)
+    print("======OG CALIBRATION======")
+    # get_error(cam, dataset_path, model, "og_")
+    # gen_camera_matrix(cam)
+    print("========NEW CALIBRATION=====")
+    get_error(cam, dataset_path, model, "calibrated_")
 
-    train_data, train_dataloader = load_dataset(path=dataset_path, batch_size=32, shuffle=True, drop_last=False, num_workers=0)
+def get_error(cam, dataset_path, model, filename):
+    
+    train_dataloader = load_dataset(path=dataset_path, batch_size=32, shuffle=True, drop_last=False, num_workers=0)
 
     train_features, train_labels = next(iter(train_dataloader))
     print(f"Feature batch shape: {train_features.size()}")
     print(f"Labels batch shape: {train_labels.size()}")
+    print(f"Length: {len(train_dataloader)}")
 
-    for i in range(10):
-        print(i, "===============")
-        img = np.array(train_features[i].squeeze())
-        label = np.array(train_labels[i])
-        # print(np.shape(img))
+    total_err = 0
+    # for data in train_dataloader:
+    for i in range(1):
+        train_features, train_labels = next(iter(train_dataloader))
 
-        results = model(img)
-        results = results.pandas().xyxy[0].to_dict(orient="records")
-        # print('results', results)
-        print('label', label)
-        if not np.any(label):
-            continue
+        for i in range(10):
+            img = np.array(train_features[i].squeeze())
+            label = np.array(train_labels[i])
 
-        img_x, img_y = cam.point_from_xyz(label)
-        rgb_img = cv2.cvtColor(img,cv2.COLOR_GRAY2RGB)
-        cv2.circle(rgb_img, (img_x, img_y), radius=2, color=(0, 255, 0), thickness=1)
-        for result in results:
-            if result['name'] != 'person':
-                continue
-
-            xmin, ymin, xmax, ymax = int(result['xmin']), int(result['ymin']), int(result['xmax']), int(result['ymax'])
-            cv2.rectangle(rgb_img, (xmin, ymin), (xmax, ymax), (255, 0, 0), 2)
-            coords = cam.xyz_from_bb((xmin, ymin, xmax, ymax))
-            print('coords', coords)
-            print('xy', xmin, ymin, xmax, ymax)
+            results = model(img)
+            results = results.pandas().xyxy[0].to_dict(orient="records")
             
+            if not np.any(label):
+                continue
         
-        cv2.imwrite(f'test_cv_{i}.png', rgb_img)
+            print(i, "===============")
+            print('label', label)
+
+            img_x, img_y = cam.point_from_xyz(label)
+            rgb_img = cv2.cvtColor(img,cv2.COLOR_GRAY2RGB)
+            cv2.circle(rgb_img, (img_x, img_y), radius=2, color=(0, 255, 0), thickness=1)
+            for result in results:
+                if result['name'] != 'person':
+                    continue
+
+                xmin, ymin, xmax, ymax = int(result['xmin']), int(result['ymin']), int(result['xmax']), int(result['ymax'])
+                cv2.rectangle(rgb_img, (xmin, ymin), (xmax, ymax), (255, 0, 0), 2)
+                coords = cam.xyz_from_bb((xmin, ymin, xmax, ymax))
+                print('coords', coords)
+
+                total_err += np.linalg.norm(label[:3] - coords)
+
+            cv2.imwrite(f'{filename}_{i}.png', rgb_img)
+    print('total err', total_err)
+
+    # data = csv.DictReader(open('src/camera_calibration/ground_truth_pose.csv', mode='r'))
+    # for d in data:
+    #     coords = np.array([d['x'], d['y'], d['z'], 0], dtype=np.float32)
+    #     calc_x, calc_y = cam.point_from_xyz(coords)
+    #     print("==============")
+    #     print("calc", calc_x, calc_y)
+    #     print(d['img_x'], d['img_y'])
 
 
 if __name__ == "__main__":
     test_camera()
+    # gen_camera_matrix()
